1d829c4ba2f124403be2ba237174b05aca797855
Pavel <mymailppa@gmail.com>
2024-02-06 13:20:54 +0300

    added operators

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Button, Display, Main } from './Styles/Styles';
 
-const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '=', 'C'];
 
 export const App = () => {
 	const [operand1, setOperand1] = useState('');
@@ -9,69 +9,51 @@ export const App = () => {
 	const [operand2, setOperand2] = useState('');
 	const [isResult, setIsResult] = useState(false);
 
-	const calculateNumbers = () => {
-		switch (operator) {
-			case '+':
-				setOperand1(parseInt(operand1) + parseInt(operand2));
-				setOperand2('');
-				setOperator('');
-				setIsResult(false);
-				break;
-			case '-':
-				setOperand1(parseInt(operand1) - parseInt(operand2));
-				setOperand2('');
-				setOperator('');
-				setIsResult(false);
-				break;
-			default:
-				setIsResult(true);
-				break;
+	const handleClick = (value) => {
+		if (value === 'C') {
+			setOperand1('');
+			setOperator('');
+			setOperand2('');
+			setIsResult(false);
+		} else if (value === '+' || value === '-') {
+			operand1 && setOperator(value);
+			setIsResult(false);
+		} else if (value === '=') {
+			if (operator === '+') {
+				operand2 && setOperand1(parseInt(operand1) + parseInt(operand2));
+			} else if (operator === '-') {
+				operand2 && setOperand1(parseInt(operand1) - parseInt(operand2));
+			}
+			setOperand2('');
+			setOperator('');
+			operand2 && setIsResult(true);
+		} else {
+			operator
+				? setOperand2((prev) => (prev === '0' ? value : prev + value))
+				: setOperand1((prev) => (prev === '0' ? value : prev + value));
+
+			setIsResult(false);
 		}
 	};
 
 	return (
 		<Main>
-			<h1>Калькулятор </h1>
+			<h1>Калькулятор</h1>
+
 			<Display className={isResult ? 'active' : ''}>
 				<h2>{[operand1, operator, operand2]}</h2>
 			</Display>
 			<div className="number-pad">
 				{NUMS.map((button) => (
 					<Button
-						onClick={(e) => {
-							operator
-								? setOperand2((prev) =>
-										prev === '0' ? e.target.textContent : prev + e.target.textContent,
-									)
-								: setOperand1((prev) =>
-										prev === '0' ? e.target.textContent : prev + e.target.textContent,
-									);
-						}}
 						key={button}
+						onClick={() => {
+							handleClick(button);
+						}}
 					>
 						{button}
 					</Button>
 				))}
-
-				<Button onClick={() => operand1 && [setOperator('+'), setIsResult(false)]}>
-					+
-				</Button>
-				<Button onClick={() => operand1 && [setOperator('-'), setIsResult(false)]}>
-					-
-				</Button>
-				<Button onClick={() => operand2 && [calculateNumbers(), setIsResult(true)]}>
-					=
-				</Button>
-				<Button
-					onClick={() => [
-						setOperator(''),
-						setOperand1(''),
-						setOperand2(''),
-						setIsResult(false),
-					]}
-				>
-					C
-				</Button>
 			</div>
 		</Main>
 	);
